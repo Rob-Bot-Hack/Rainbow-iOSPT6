4dc80d9c2cc082a39e9b74cdd8ea085b407c1f5e
Robert Vance <Fearmydotzs@gmail.com>
2020-04-07 21:24:27 -0600

    Finished Project

---
+++ Rainbow iOSPT6/Model/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rainbow iOSPT6
+//
+//  Created by Rob Vance on 4/7/20.
+//  Copyright © 2020 Robs Creations. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    var colorName: String
+    var color: UIColor
+}
